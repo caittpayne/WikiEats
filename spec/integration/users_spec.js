b7355cb9f696dbc491896f3a2ada177a486f8abf
Cait Payne <caitpayne@outlook.com>
2018-11-16 16:10:19 -0700

    Sendgrid implentation complete

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -1,8 +1,20 @@
 const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:3000/users/';
+const User = require('../../src/db/models').User;
+const sequelize = require('../../src/db/models/index').sequelize;
 
 describe('routes : users', () => {
+    beforeEach((done) => {
+        sequelize.sync({force: true})
+        .then(() => {
+            done();
+        })
+        .catch((err) => {
+            console.log(err);
+            done();
+        });
+    });
 
     describe('GET /users/signup', () => {
         it('should render a view of the sign-up form', (done) => {
@@ -10,6 +22,71 @@ describe('routes : users', () => {
                 expect(err).toBeNull();
                 expect(body).toContain('Choose from one of our plans to start collaborating');
                 done();
+            });
+        });
+    });
+    describe('POST /users', () => {
+        it('should create a new user with valid values and redirect', (done) => {
+            const options = {
+                url: base,
+                form: {
+                    email: "user@email.com",
+                    password: '1234567',
+                    name: 'Bob'
+                }
+            }
+            request.post(options, (err, res, body) => {
+                User.findOne({where: {email: "user@email.com"}})
+                .then((user) => {
+                    expect(user).not.toBeNull();
+                    expect(user.email).toBe('user@email.com');
+                    expect(user.name).toBe('Bob');
+                    expect(user.id).toBe(1);
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+        it('should not create a new user with invalid attributes and redirect', (done) => {
+            request.post({
+                url: base,
+                form: {
+                    email: 'user',
+                    password: '1234567',
+                    name: "Bob"
+                }
+            }, (err, res, body) => {
+                User.findOne({where: {email: 'user'}})
+                .then((user) => {
+                    expect(user).toBeNull();
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+        it('should not create a new user with missing attributes and redirect', (done) => {
+            request.post({
+                url: base,
+                form: {
+                    email: 'user@email.com',
+                    password: '1234567'
+                }
+            }, (err, res, body) => {
+                User.findOne({where: {email: 'user@email.com'}})
+                .then((user) => {
+                    expect(user).toBeNull();
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
             });
         });
     });
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,31 @@
+const userQueries = require('../db/queries.users.js');
+const passport = require('passport');
 module.exports = {
 
     signup(req, res, next) {
         res.render('users/signup');
-      }
+      },
+
+    create(req, res, next) {
+
+        let newUser = {
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirmation: req.body.passwordConfirmation,
+            name: req.body.name
+        };
+
+        userQueries.createUser(newUser, (err, user) => {
+            if(err) {
+                req.flash('error', err);
+                res.redirect('/users/signup');
+            } else {
+                
+                passport.authenticate('local')(req, res, () => {
+                    req.flash('notice', 'Welcome to WikiEats!'); 
+                    res.redirect('/')
+                });
+            }
+        });
+    },
 }
