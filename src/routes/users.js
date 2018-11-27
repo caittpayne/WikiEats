e6248cd962448feaf10c788f69df478020ce74c8
Cait Payne <caitpayne@outlook.com>
2018-11-27 12:27:17 -0700

    Added signin functionality

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,9 @@ const validation = require('./validation');
 
 
 router.get('/users/signup', userController.signup);
+router.get('/users/signin', userController.signInForm);
+router.get('/users/signout', userController.signOut);
 router.post('/users', validation.validateUsers, userController.create);
+router.post('/users/signin', validation.validateUsers, userController.signIn);
 
 module.exports = router;
